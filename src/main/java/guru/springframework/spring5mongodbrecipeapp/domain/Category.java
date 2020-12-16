cd73de33b9a68fd6e9d12456ca82fdfacac5c552
EhsanRP <sir.ehrp@gmail.com>
2020-12-16 22:29:19 +0330

    Db Doesn't work

--- src/main/java/guru/springframework/spring5mongodbrecipeapp/domain/Category.java
+++ src/main/java/guru/springframework/spring5mongodbrecipeapp/domain/Category.java
@@ -12,7 +12,6 @@ import java.util.Set;
 @Getter
 @Setter
 @Document
-
 public class Category {
 
     @Id
--- src/test/java/guru/springframework/spring5mongodbrecipeapp/converters/NotesToNotesCommandTest.java
+++ src/test/java/guru/springframework/spring5mongodbrecipeapp/converters/NotesToNotesCommandTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/
+
 public class NotesToNotesCommandTest {
 
     public static final String ID_VALUE = "1";
